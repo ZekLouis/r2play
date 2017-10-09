eb4d3c369705edb104e2e715dca9f5189d7b1af0
Romain Chemartin <romain.chemartin@etu.unilim.fr>
2017-10-09 16:59:00 +0200

    add musics

--- assets/playlists/playlists.js
+++ assets/playlists/playlists.js
@@ -3,34 +3,34 @@ var playlists = [
     "title" : "Good Morning",
     "music" : [
       {
-        "src" : "assets/music/vald1.mp3",
-        "img" : "assets/cover/agartha.jpg",
-        "titre" : "Strip",
-        "artiste" : "Vald"
+        "src" : "assets/music/absent_heavens.wav",
+        "img" : "assets/cover/absent-faith.jpg",
+        "titre" : "Heavens",
+        "artiste" : "Absent Faith"
       },
       {
-        "src" : "assets/music/vald2.mp3",
-        "img" : "assets/cover/nqnt.jpg",
-        "titre" : "Je t'aime",
-        "artiste" : "Vald"
+        "src" : "assets/music/sekai_somebody.mp3",
+        "img" : "assets/cover/sekai.jpg",
+        "titre" : "Somebody",
+        "artiste" : "Sekai"
       },
       {
-        "src" : "assets/music/test.m4a",
-        "img" : "assets/cover/nqnt.jpg",
-        "titre" : "33333",
-        "artiste" : "Valdqsdqd"
+        "src" : "assets/music/elektromania_sky-high.mp3",
+        "img" : "assets/cover/elektromania.jpg",
+        "titre" : "Sky High",
+        "artiste" : "Elektromania"
       },
       {
-        "src" : "assets/music/test.m4a",
-        "img" : "assets/cover/nqnt.jpg",
-        "titre" : "Je 444444444444'aime",
-        "artiste" : "Vijdsqijald"
+        "src" : "assets/music/raven&kreyn_in-the-air.mp3",
+        "img" : "assets/cover/intheair.jpg",
+        "titre" : "In the air",
+        "artiste" : "Raven & Kreyn"
       },
       {
-        "src" : "assets/music/test.m4a",
-        "img" : "assets/cover/nqnt.jpg",
-        "titre" : "Je 55555555",
-        "artiste" : "Vaoijoijooijld"
+        "src" : "assets/music/jjd_adventure.mp3",
+        "img" : "assets/cover/jjd.jpg",
+        "titre" : "Adventure",
+        "artiste" : "JJD"
       },
     ]
   },
@@ -38,16 +38,22 @@ var playlists = [
     "title" : "Good Afternoon",
     "music" : [
       {
-        "src" : "assets/music/vald1.mp3",
-        "img" : "assets/cover/agartha.jpg",
-        "titre" : "Strip",
-        "artiste" : "Vald"
+        "src" : "assets/music/anikdote_life-is-over.mp3",
+        "img" : "assets/cover/anikdote.jpg",
+        "titre" : "Life is over",
+        "artiste" : "Anikdote"
       },
       {
-        "src" : "assets/music/vald2.mp3",
-        "img" : "assets/cover/nqnt.jpg",
-        "titre" : "Je t'aime",
-        "artiste" : "Vald"
+        "src" : "assets/music/lfz_echoes.mp3",
+        "img" : "assets/cover/lfz.jpg",
+        "titre" : "Echoes",
+        "artiste" : "LFZ"
+      },
+      {
+        "src" : "assets/music/jordan-schor_cosmic.mp3",
+        "img" : "assets/cover/jordanschor.jpg",
+        "titre" : "Cosmic",
+        "artiste" : "Jordan Schor"
       },
     ]
   },
@@ -55,16 +61,28 @@ var playlists = [
     "title" : "Good Night",
     "music" : [
       {
-        "src" : "assets/music/vald1.mp3",
-        "img" : "assets/cover/agartha.jpg",
-        "titre" : "Strip",
-        "artiste" : "Vald"
+        "src" : "assets/music/desmeone_undone.mp3",
+        "img" : "assets/cover/desmeone.jpg",
+        "titre" : "Undone",
+        "artiste" : "Desmeone"
       },
       {
-        "src" : "assets/music/vald2.mp3",
-        "img" : "assets/cover/nqnt.jpg",
-        "titre" : "Je t'aime",
-        "artiste" : "Vald"
+        "src" : "assets/music/retrovision_campfire.mp3",
+        "img" : "assets/cover/retrovision.jpg",
+        "titre" : "Campfire",
+        "artiste" : "RetroVision"
+      },
+      {
+        "src" : "assets/music/sekai_somebody.mp3",
+        "img" : "assets/cover/sekai.jpg",
+        "titre" : "Somebody",
+        "artiste" : "Sekai"
+      },
+      {
+        "src" : "assets/music/anikdote_life-is-over.mp3",
+        "img" : "assets/cover/anikdote.jpg",
+        "titre" : "Life is over",
+        "artiste" : "Anikdote"
       },
     ]
   },
@@ -72,16 +90,28 @@ var playlists = [
     "title" : "Sport Motivation",
     "music" : [
       {
-        "src" : "assets/music/vald1.mp3",
-        "img" : "assets/cover/agartha.jpg",
-        "titre" : "Strip",
-        "artiste" : "Vald"
+        "src" : "assets/music/jordan-schor_cosmic.mp3",
+        "img" : "assets/cover/jordanschor.jpg",
+        "titre" : "Cosmic",
+        "artiste" : "Jordan Schor"
       },
       {
-        "src" : "assets/music/vald2.mp3",
-        "img" : "assets/cover/nqnt.jpg",
-        "titre" : "Je t'aime",
-        "artiste" : "Vald"
+        "src" : "assets/music/retrovision_campfire.mp3",
+        "img" : "assets/cover/retrovision.jpg",
+        "titre" : "Campfire",
+        "artiste" : "RetroVision"
+      },
+      {
+        "src" : "assets/music/sekai_somebody.mp3",
+        "img" : "assets/cover/sekai.jpg",
+        "titre" : "Somebody",
+        "artiste" : "Sekai"
+      },
+      {
+        "src" : "assets/music/anikdote_life-is-over.mp3",
+        "img" : "assets/cover/anikdote.jpg",
+        "titre" : "Life is over",
+        "artiste" : "Anikdote"
       },
     ]
   },
@@ -89,16 +119,28 @@ var playlists = [
     "title" : "Coffee Break",
     "music" : [
       {
-        "src" : "assets/music/vald1.mp3",
-        "img" : "assets/cover/agartha.jpg",
-        "titre" : "Strip",
-        "artiste" : "Vald"
+        "src" : "assets/music/anikdote_life-is-over.mp3",
+        "img" : "assets/cover/anikdote.jpg",
+        "titre" : "Life is over",
+        "artiste" : "Anikdote"
       },
       {
-        "src" : "assets/music/vald2.mp3",
-        "img" : "assets/cover/nqnt.jpg",
-        "titre" : "Je t'aime",
-        "artiste" : "Vald"
+        "src" : "assets/music/retrovision_campfire.mp3",
+        "img" : "assets/cover/retrovision.jpg",
+        "titre" : "Campfire",
+        "artiste" : "RetroVision"
+      },
+      {
+        "src" : "assets/music/elektromania_sky-high.mp3",
+        "img" : "assets/cover/elektromania.jpg",
+        "titre" : "Sky High",
+        "artiste" : "Elektromania"
+      },
+      {
+        "src" : "assets/music/raven&kreyn_in-the-air.mp3",
+        "img" : "assets/cover/intheair.jpg",
+        "titre" : "In the air",
+        "artiste" : "Raven & Kreyn"
       },
     ]
   },
@@ -106,16 +148,28 @@ var playlists = [
     "title" : "Morning Routine",
     "music" : [
       {
-        "src" : "assets/music/vald1.mp3",
-        "img" : "assets/cover/agartha.jpg",
-        "titre" : "Strip",
-        "artiste" : "Vald"
+        "src" : "assets/music/elektromania_sky-high.mp3",
+        "img" : "assets/cover/elektromania.jpg",
+        "titre" : "Sky High",
+        "artiste" : "Elektromania"
       },
       {
-        "src" : "assets/music/vald2.mp3",
-        "img" : "assets/cover/nqnt.jpg",
-        "titre" : "Je t'aime",
-        "artiste" : "Vald"
+        "src" : "assets/music/absent_heavens.wav",
+        "img" : "assets/cover/absent-faith.jpg",
+        "titre" : "Heavens",
+        "artiste" : "Absent Faith"
+      },
+      {
+        "src" : "assets/music/raven&kreyn_in-the-air.mp3",
+        "img" : "assets/cover/intheair.jpg",
+        "titre" : "In the air",
+        "artiste" : "Raven & Kreyn"
+      },
+      {
+        "src" : "assets/music/sekai_somebody.mp3",
+        "img" : "assets/cover/sekai.jpg",
+        "titre" : "Somebody",
+        "artiste" : "Sekai"
       },
     ]
   },
